b528611ec4cb4362811278753bbe4433bffa0027
najoon <sk123928@gmail.com>
2022-05-30 17:36:40 +0900

    asdf

--- project_sellpit/src/main/java/com/callor/school/pesistance/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.callor.school.pesistance;
-
-import com.callor.school.model.UserVO;
-
-public interface UserDao extends GenericDao<UserVO, String>{
-
-}
--- project_sellpit/src/main/java/com/callor/school/service/StudentService.java
+++
@@ -1,8 +0,0 @@
-package com.callor.school.service;
-
-import com.callor.school.model.StudentVO;
-import com.callor.school.pesistance.GenericDao;
-
-public interface StudentService extends GenericDao<StudentVO, String>{
-
-}
