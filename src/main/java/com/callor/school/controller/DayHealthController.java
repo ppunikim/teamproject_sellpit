4fc536971dd683d209694fe84fc348566ad3bb85
ppunikim <yd62322@naver.com>
2022-06-14 08:13:56 +0900

    a

--- src/main/java/com/callor/school/controller/DayHealthController.java
+++ src/main/java/com/callor/school/controller/DayHealthController.java
@@ -2,13 +2,15 @@ package com.callor.school.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+@Controller
+@RequestMapping(value="/user")
 public class DayHealthController {
 
 	@RequestMapping(value = "/dayHealth", method = RequestMethod.GET)
-	public String dayHealth(HttpSession session) {
+	public String dayHealth(HttpSession session ) {
 		
 		return null;
 	}
