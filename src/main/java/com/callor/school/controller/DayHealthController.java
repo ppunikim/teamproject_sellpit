6565f5041226c76c02f632702f54144567a40f62
ppunikim <yd62322@naver.com>
2022-06-13 23:22:58 +0900

    dayhealth 만들 준비

--- src/main/java/com/callor/school/controller/DayHealthController.java
+++ src/main/java/com/callor/school/controller/DayHealthController.java
@@ -2,9 +2,12 @@ package com.callor.school.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@RequestMapping(value="/user")
+@Controller
 public class DayHealthController {
 
 	@RequestMapping(value = "/dayHealth", method = RequestMethod.GET)
@@ -13,3 +16,4 @@ public class DayHealthController {
 		return null;
 	}
 }
+                   
