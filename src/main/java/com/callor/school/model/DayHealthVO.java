a3f6f73698906e47fcc31f316621273c7448473a
ppunikim <yd62322@naver.com>
2022-06-15 15:58:44 +0900

    today

--- src/main/java/com/callor/school/model/DayHealthVO.java
+++ src/main/java/com/callor/school/model/DayHealthVO.java
@@ -16,12 +16,13 @@ import lombok.ToString;
 public class DayHealthVO {
 	
 
-	private String my_seq	;//		NUMBER			PRIMARY KEY,
-	private String my_username		;//nVARCHAR2(50)	NOT NULL,
-		private String my_date			;//VARCHAR2(15)	NOT NULL,
-		private String my_dayset		;//nVARCHAR2(30)	NOT NULL,
-		private String my_weight		;//NUMBER			NOT NULL,
-	private String my_rep			;//NUMBER			NOT NULL,
+	private String my_seq;//		NUMBER			PRIMARY KEY,
+	private String my_username;//nVARCHAR2(50)	NOT NULL,
+	private String my_list;
+	private String my_date;//VARCHAR2(15)	NOT NULL,
+	private String my_dayset;//nVARCHAR2(30)	NOT NULL,
+	private String my_weight;//NUMBER			NOT NULL,
+	private String my_rep;//NUMBER			NOT NULL,
 
 
 }
